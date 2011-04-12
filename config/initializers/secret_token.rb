e3dc2e9990b8c674031e04d85b1130f188977244
Karl Brightman <karl@karlbright.org>
2011-04-12 09:05:18 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dubstepmakesyou::Application.config.secret_token = '950abf20284077c1ab32c057f5ed2f0fbe8752c292ac479732900ade260257e1b083095d3bbe9fe9791b760937c849f360a6d8a1f124f74010228b390e8e4d52'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Dubstepmakesyou::Application.config.session_store :cookie_store, :key => '_dubstepmakesyou_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Dubstepmakesyou::Application.config.session_store :active_record_store
